d03b6fdda51eb8c5e5095556f2f7bfac63058bc6
ggec <zhouhmgz@gmail.com>
2017-08-01 17:19:53 +0800

    remove code

--- app/audio/EnumRecordingState.java
+++
@@ -1,9 +0,0 @@
-package com.ggec.voice.assistservice.audio;
-
-/**
- * Created by ggec on 2017/3/29.
- */
-
-public enum EnumRecordingState {
-    recording, cancel, stop, ready
-}
--- avs/src/main/java/com/amazon/alexa/avs/wakeword/WakeWordDetectedHandler.java
+++
@@ -1,5 +0,0 @@
-package com.amazon.alexa.avs.wakeword;
-
-public interface WakeWordDetectedHandler {
-    void onWakeWordDetected();
-}
